6b3b0d1890dbab73d30476aafba5c556a3da2d0d
Tales Pinheiro Vasconcelos <tales.pv@hotmail.com>
2016-07-22 11:44:40 -0300

    ESTATÍSTICA DESCRITIVA
    Alteração do tipo de arquivo de .r para .Rmd

--- ESTATÍSTICA ESCRITIVA (.Rmd)/ED6.r
+++ ESTATÍSTICA ESCRITIVA (.Rmd)/ED6.r
@@ -1,4 +1,5 @@
-Discos de alumínio são produzidos e a cada 15 minutos 3 discos são retirados da linha e pesados. Os dados obtidos durante um período de quase 7 horas estão apresentados na tabela abaixo.
+Discos de alumínio são produzidos e a cada 15 minutos 3 discos são retirados da linha e pesados. 
+Os dados obtidos durante um período de quase 7 horas estão apresentados na tabela abaixo.
 
 a) Estime e analise criticamente as estatísticas de média e variância do peso dos discos de alumínio (g).
 
@@ -20,7 +21,9 @@ plot(media.peso.por.amostra, main = "Série Temporal",
      xlab = "Amostra", ylab = "Massa/g", pch = 20)
 ```
 
-*O histograma das médias dos pesos das amostras de discos de alumínio apresenta uma provável normalidade na distribuição dos dados em torno da média. Já o gráfico de série temporal indica uma possível correlação crescente entre o peso do disco de alumínio e a amostra*
+*O histograma das médias dos pesos das amostras de discos de alumínio apresenta uma provável normalidade 
+na distribuição dos dados em torno da média. Já o gráfico de série temporal indica uma possível correlação 
+crescente entre o peso do disco de alumínio e a amostra*
 
 ```{r, fig.height= 5, fig.width= 11}
 dp.peso.por.amostra = by(AlDisc$Peso, as.factor(AlDisc$Amostra), sd.default)
@@ -33,6 +36,6 @@ plot(dp.peso.por.amostra, main = "Série Temporal",
      xlab = "Amostra", ylab = "dp/g", pch=20, col = "blue")
 ```
 
-*O histograma dos desvios padrões dos pesos das amostras de discos de alumínio apresenta um certo desvio da normalidade com um alongamento da cauda à esquerda (concentração da dados à direita). Já o gráfico de série temporal indica uma provável aleatoriedade dos desvios das amostras de disco de alumínio*
-
-\newpage
+*O histograma dos desvios padrões dos pesos das amostras de discos de alumínio apresenta um certo desvio da 
+normalidade com um alongamento da cauda à esquerda (concentração da dados à direita). Já o gráfico de série 
+temporal indica uma provável aleatoriedade dos desvios das amostras de disco de alumínio*
